f01979f41ee94b33b6a4b7911e8f58228e01efd2
Wilson Chang <ckwojai@gmail.com>
2017-11-16 16:56:28 -0800

    Lab 2 Initialization + Library Implementation

---
+++ Lab 2/Arduino Code/test/test.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <sensor_fusion.h>
+  int a = 0;
+  int b = 0;
+  int c = 0;
+void setup()
+{
+  const unsigned int CONFIG = 26;
+  const unsigned int GYRO_CONFIG = 27;
+  const unsigned int PWR_MGMT_1 = 107;
+  Wire.begin();        // join i2c bus (address optional for master)
+  readReg(PWR_MGMT_1, a, 1);
+  readReg(GYRO_CONFIG, b, 1);
+  readReg(CONFIG, c, 1);
+  writeReg(PWR_MGMT_1, a & 0b10111111, 1);	       /* PWR_MGMT_1: seoncd bit to 0 */
+  writeReg(GYRO_CONFIG, b | 0b00011000, 1);	       /* PWR_MGMT_1 4 and 5 bit to 1*/
+  writeReg(CONFIG, c & 0b11111000, 1);	       /* PWR_MGMT_1 4 and 5 bit to 1*/
+  readReg(PWR_MGMT_1, a, 1);
+  readReg(GYRO_CONFIG, b, 1);
+  readReg(CONFIG, c, 1);
+  Serial.begin(9600);  // start serial for output
+
+}
+
+void loop()
+{
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println(c);
+  Serial.println("idk");
+  delay(1000);
+  /* while(Wire.available())    // slave may send less than requested */
+  /* {  */
+  /*   char c = Wire.read();    // receive a byte as character */
+  /*   Serial.print(c);         // print the character */
+  /* } */
+
+  /* delay(500); */
+}
