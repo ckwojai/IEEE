f9255eabe6998b89dd0ca3dc4ea3acb68c80acf6
Wilson Chang <ckwojai@gmail.com>
2017-11-17 22:05:10 -0800

    Update

--- Lab 2/Arduino Code/bias/bias.ino
+++ Lab 2/Arduino Code/bias/bias.ino
@@ -36,7 +36,7 @@ void setup()
   config = config & 0b11111000;
   writeReg(PWR_MGMT_1, &pwr, 1);
   writeReg(GYRO_CONFIG, &gyro, 1);
-  writeReg(PWR_MGMT_1, &config, 1);
+  writeReg(CONFIG, &config, 1);
   /* calculate bias */
   while (count < N) {
     uint8_t status = 0;
@@ -86,36 +86,49 @@ void setup()
       z_gyr_arr[count] = z_g;
       temp = 0;
       /* INITIALIZATION */
+      x_a = 0;
+      y_a = 0;
+      z_a = 0;
+      x_g = 0;
+      y_g = 0;
+      z_g = 0;
       count++;
     }
-    long int x_avg_a = 0;
-    long int y_avg_a = 0;
-    long int z_avg_a = 0;
-    long int x_avg_g = 0;
-    long int y_avg_g = 0;
-    long int z_avg_g = 0;
-    for (int i = 0; i < N; i++) {
-      x_avg_a += x_acc_arr[i];
-      y_avg_a += y_acc_arr[i];
-      z_avg_a += z_acc_arr[i];
-      x_avg_g += x_gyr_arr[i];
-      y_avg_g += y_gyr_arr[i];
-      z_avg_g += z_gyr_arr[i];
-    }
-    bias_x_a = 0 - x_avg_a / N;
-    bias_y_a = 0 - y_avg_a / N;
-    bias_z_a = 16384 - z_avg_a / N;
-    bias_x_g = 0 - x_avg_g / N;
-    bias_y_g = 0 - y_avg_g / N;
-    bias_z_g = 0 - z_avg_g / N;
   }
-}  
+  long int x_avg_a = 0;
+  long int y_avg_a = 0;
+  long int z_avg_a = 0;
+  long int x_avg_g = 0;
+  long int y_avg_g = 0;
+  long int z_avg_g = 0;
+  for (int i = 0; i < N; i++) {
+    x_avg_a += x_acc_arr[i];
+    y_avg_a += y_acc_arr[i];
+    z_avg_a += z_acc_arr[i];
+    x_avg_g += x_gyr_arr[i];
+    y_avg_g += y_gyr_arr[i];
+    z_avg_g += z_gyr_arr[i];
+  }
+  bias_x_a = x_avg_a / N;
+  bias_y_a = y_avg_a / N;
+  bias_z_a = z_avg_a / N - 16384;
+  bias_x_g = x_avg_g / N;
+  bias_y_g = y_avg_g / N;
+  bias_z_g = z_avg_g / N;
+}
 void loop()
 {
   uint8_t status = 0;
   uint8_t temp = 0;
   readReg(INT_STATUS, &status, 1);
   if (status & 0b00000001 == 1) { /* LSB is 1 --> data ready*/
+    /* resetting values */
+    x_a = 0;
+    y_a = 0;
+    z_a = 0;
+    x_g = 0;
+    y_g = 0;
+    z_g = 0;
     /* getting ACCEL_X */
     readReg(ACCEL_XOUT_1, &temp, 1); /* read the first 8 bit */
     x_a = (x_a | temp) << 8;
@@ -152,39 +165,40 @@ void loop()
     readReg(GYRO_ZOUT_0, &temp, 1); /* read the second 8 bit and combine to become a 16 bit */
     z_g = z_g | temp;
     temp = 0;
+
     /* Account for bias */
-    /* x_a = x_a + bias_x_a; */
-    /* y_a = y_a + bias_y_a; */
-    /* z_a = z_a + bias_z_a; */
-    /* x_g = x_g + bias_x_g; */
-    /* y_g = y_g + bias_y_g; */
-    /* z_g = z_g + bias_z_g; */
+    /* x_a = x_a - bias_x_a; */
+    /* y_a = y_a - bias_y_a; */
+    /* z_a = z_a - bias_z_a; */
+    /* x_g = x_g - bias_x_g; */
+    /* y_g = y_g - bias_y_g; */
+    /* z_g = z_g - bias_z_g; */
     /* Bias */
-    /* Serial.print(bias_x_a); */
-    /* Serial.print(" "); */
-    /* Serial.print(bias_y_a); */
-    /* Serial.print(" "); */
-    /* Serial.print(bias_z_a); */
-    /* Serial.print(" "); */
-    /* Serial.print(bias_x_g); */
-    /* Serial.print(" "); */
-    /* Serial.print(bias_y_g); */
-    /* Serial.print(" "); */
-    /* Serial.print(bias_z_g); */
-    /* Serial.println(""); */
-    /* Real Data */
-    Serial.print(x_a);
+    Serial.print(bias_x_a);
     Serial.print(" ");
-    Serial.print(y_a);
+    Serial.print(bias_y_a);
     Serial.print(" ");
-    Serial.print(z_a);
+    Serial.print(bias_z_a);
     Serial.print(" ");
-    Serial.print(x_g);
+    Serial.print(bias_x_g);
     Serial.print(" ");
-    Serial.print(y_g);
+    Serial.print(bias_y_g);
     Serial.print(" ");
-    Serial.print(z_g);
+    Serial.print(bias_z_g);
     Serial.println("");
+    /* Real Data */
+    /* Serial.print(x_a); */
+    /* Serial.print(" "); */
+    /* Serial.print(y_a); */
+    /* Serial.print(" "); */
+    /* Serial.print(z_a); */
+    /* Serial.print(" "); */
+    /* Serial.print(x_g); */
+    /* Serial.print(" "); */
+    /* Serial.print(y_g); */
+    /* Serial.print(" "); */
+    /* Serial.print(z_g); */
+    /* Serial.println(""); */
   }
 
   delay(1000);
