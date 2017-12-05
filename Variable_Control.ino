d73669034394427d55b98cccdb2e83d404c8545b
Mip2828 <33337278+Mip2828@users.noreply.github.com>
2017-12-04 16:55:50 -0800

    Variable Control Arduino Code

---
+++ Variable_Control.ino
@@ -0,0 +1,21 @@
+
+#define PWM 3
+const int UPPERBOUND  =  100;
+const int ZERO = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < UPPERBOUND; i++)
+  {
+    analogWrite(PWM, i);
+  }
+  delay(50);
+  for (int i = 100; i > 0; i--) {
+    analogWrite(PWM, i);
+  }
+  delay(50);
+}
